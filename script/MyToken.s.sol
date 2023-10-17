1fa07cb37d5fe75c2e7dc1e8f8461fefdd43d3b2
Eric Lau <ericglau@outlook.com>
2023-10-17 17:55:57 -0400

    Test using script with broadcast

--- script/MyToken.s.sol
+++ script/MyToken.s.sol
@@ -2,15 +2,27 @@
 pragma solidity ^0.8.20;
 
 import "forge-std/Script.sol";
-import "../src/MyToken.sol";
+import {MyToken} from "../src/MyToken.sol";
+import {MyTokenV2} from "../src/MyTokenV2.sol";
+
+import {Upgrades} from "@openzeppelin/foundry-upgrades/Upgrades.sol";
 
 contract MyTokenScript is Script {
   function setUp() public {}
 
   function run() public {
     vm.startBroadcast();
-    MyToken instance = new MyToken();
-    console.log("Contract deployed to %s", address(instance));
+
+    address proxy = address(Upgrades.deployTransparentProxy(type(MyToken).creationCode, msg.sender, abi.encodeCall(MyToken.initialize, ("hello", msg.sender))));
+    Upgrades.upgradeProxy(proxy, type(MyTokenV2).creationCode, msg.sender, abi.encodeCall(MyTokenV2.resetGreeting, ()));
+
+    address beacon = address(Upgrades.deployBeacon(type(MyToken).creationCode, msg.sender));
+    Upgrades.upgradeBeacon(beacon, type(MyTokenV2).creationCode, msg.sender);    
+
     vm.stopBroadcast();
   }
 }
+
+
+
+
