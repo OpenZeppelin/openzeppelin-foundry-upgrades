dbdd7721b1f23849ebcd05b05fc2ce18dd931d9d
Eric Lau <ericglau@outlook.com>
2023-10-25 11:17:09 -0400

    Fix linter

---
+++ solhint.config.js
@@ -0,0 +1,12 @@
+const rules = [
+  'no-unused-vars',
+  'const-name-snakecase',
+  'contract-name-camelcase',
+  'event-name-camelcase',
+  'func-name-mixedcase',
+  'func-param-name-mixedcase',
+  'modifier-name-mixedcase',
+  'var-name-mixedcase',
+  'imports-on-top',
+  'no-global-import',
+];
