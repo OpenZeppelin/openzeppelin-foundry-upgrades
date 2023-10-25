c1d1a2c3ada7976178042e6981866b2821fe1299
Eric Lau <ericglau@outlook.com>
2023-10-25 11:37:09 -0400

    Use custom solhint plugin from OpenZeppelin Contracts

--- solhint.config.js
+++ solhint.config.js
@@ -1,12 +1,1 @@
-const rules = [
-  'no-unused-vars',
-  'const-name-snakecase',
-  'contract-name-camelcase',
-  'event-name-camelcase',
-  'func-name-mixedcase',
-  'func-param-name-mixedcase',
-  'modifier-name-mixedcase',
-  'var-name-mixedcase',
-  'imports-on-top',
-  'no-global-import',
-];
+module.exports = require('./lib/openzeppelin-contracts/solhint.config.js');
