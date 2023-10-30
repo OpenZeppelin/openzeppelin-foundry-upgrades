ef284162daaba48ff7956651ec7a74861f8d1b73
Eric Lau <ericglau@outlook.com>
2023-10-30 13:09:10 -0400

    Remove OpenZeppelin Contracts from git submodules

--- solhint.config.js
+++ solhint.config.js
@@ -1,1 +1,24 @@
-module.exports = require('./lib/openzeppelin-contracts/solhint.config.js');
+const customRules = require('./scripts/solhint-custom');
+
+console.log('Using custom rules:', JSON.stringify(customRules, null, 2));
+
+const rules = [
+  'no-unused-vars',
+  'const-name-snakecase',
+  'contract-name-camelcase',
+  'event-name-camelcase',
+  'func-name-mixedcase',
+  'func-param-name-mixedcase',
+  'modifier-name-mixedcase',
+  'var-name-mixedcase',
+  'imports-on-top',
+  'no-global-import',
+  ...customRules.map(r => `openzeppelin/${r.ruleId}`),
+];
+
+console.log('Using rules:', JSON.stringify(rules, null, 2));
+
+module.exports = {
+  plugins: ['openzeppelin'],
+  rules: Object.fromEntries(rules.map(r => [r, 'error'])),
+};
