1ee550a33fd16fc00bff3d28048fe2e71702cba9
Eric Lau <ericglau@outlook.com>
2023-12-19 18:54:47 -0500

    Update script

--- test/Defender.s.sol
+++ test/Defender.s.sol
@@ -2,23 +2,12 @@
 pragma solidity ^0.8.20;
 
 import {Script} from "forge-std/Script.sol";
-import {Vm} from "forge-std/Vm.sol";
-
-import {Greeter} from "./contracts/Greeter.sol";
-import {GreeterProxiable} from "./contracts/GreeterProxiable.sol";
-import {GreeterV2} from "./contracts/GreeterV2.sol";
-import {GreeterV2Proxiable} from "./contracts/GreeterV2Proxiable.sol";
 
 import {Defender} from "openzeppelin-foundry-upgrades/Defender.sol";
-import {Utils} from "openzeppelin-foundry-upgrades/internal/Utils.sol";
 import {console} from "forge-std/console.sol";
-
-import {strings} from "solidity-stringutils/strings.sol";
 
 contract UpgradesScript is Script {
     function setUp() public {}
-
-    using strings for *;
 
     function run() public {
         string memory deployed = Defender.deployContract("MyContractFile.sol:MyContractName");
