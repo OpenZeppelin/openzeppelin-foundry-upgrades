c735d2dba589f4fcf6647d7d829259beb4794561
Eric Lau <ericglau@outlook.com>
2024-01-29 18:08:01 -0500

    Update example script

--- test/Defender.s.sol
+++ test/Defender.s.sol
@@ -2,9 +2,9 @@
 pragma solidity ^0.8.20;
 
 import {Script} from "forge-std/Script.sol";
+import {console} from "forge-std/console.sol";
 
 import {Defender} from "openzeppelin-foundry-upgrades/Defender.sol";
-import {console} from "forge-std/console.sol";
 
 /**
  * @dev Sample script to deploy a contract using Defender.
@@ -13,7 +13,7 @@ contract DefenderScript is Script {
     function setUp() public {}
 
     function run() public {
-        address deployed = Defender.deployContract("WithConstructor.sol:WithConstructor", abi.encode(123));
-        console.log("Successfully deployed to address ", deployed);
+        address deployed = Defender.deployContract("WithConstructor.sol", abi.encode(123));
+        console.log("Deployed contract to address", deployed);
     }
 }
