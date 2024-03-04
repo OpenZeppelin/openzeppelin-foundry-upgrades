c7e984a099da5fb2c104e1fe4de0b6820406eb22
Eric Lau <ericglau@outlook.com>
2024-03-04 18:17:25 -0500

    Add docgen

---
+++ docs/config.js
@@ -0,0 +1,7 @@
+/** @type import('solidity-docgen/dist/config').UserConfig */
+module.exports = {
+  outputDir: 'docs/modules/api/pages',
+  exclude: ['internal'],
+  pageExtension: '.adoc',
+  pages: 'files',
+};
