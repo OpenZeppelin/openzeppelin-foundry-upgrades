3de8be8127f29cbb0d302f750c42dbe862c04f7e
Eric Lau <ericglau@outlook.com>
2024-03-05 14:23:47 -0500

    Use template

--- docs/config.js
+++ docs/config.js
@@ -1,6 +1,7 @@
 /** @type import('solidity-docgen/dist/config').UserConfig */
 module.exports = {
   outputDir: 'docs/modules/api/pages',
+  templates: 'docs/templates',
   exclude: ['internal'],
   pageExtension: '.adoc',
   pages: 'files',
